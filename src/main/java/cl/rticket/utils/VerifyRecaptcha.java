1b6dbbdf4d9204d607616f85986841f1133fbcb5
Esteban GutierrezEsteban <egutierrez@uchile.cl>
2017-08-25 16:09:47 -0300

    agrega recaptcha

---
+++ src/main/java/cl/rticket/utils/VerifyRecaptcha.java
@@ -0,0 +1,66 @@
+package cl.rticket.utils;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+
+//http://www.journaldev.com/7133/how-to-integrate-google-recaptcha-in-java-web-application
+public class VerifyRecaptcha {
+
+	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
+	public static final String secret = "6LdfMy4UAAAAADgh7fwyO8EYcDmFFMXhlH0vB7pn";
+	
+	public static boolean verify(String gRecaptchaResponse) throws IOException {
+		
+		if (gRecaptchaResponse == null || "".equals(gRecaptchaResponse)) {
+			return false;
+		}
+		
+		try{
+			URL obj = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+			// add reuqest header
+			con.setRequestMethod("POST");
+			//con.setRequestProperty("User-Agent", USER_AGENT);
+			//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			String postParams = "secret=" + secret + "&response="+ gRecaptchaResponse;
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postParams);
+			wr.flush();
+			wr.close();
+			//int responseCode = con.getResponseCode();
+			//System.out.println("\nSending 'POST' request to URL : " + url);
+			//System.out.println("Post parameters : " + postParams);
+			//System.out.println("Response Code : " + responseCode);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			// print result
+			//System.out.println(response.toString());
+			JsonObject jobj = new Gson().fromJson(response.toString(), JsonObject.class);
+			boolean res = jobj.get("success").getAsBoolean();
+		  
+			return res;
+			}catch(Exception e){
+				e.printStackTrace();
+				return false;
+			}
+	}
+}
